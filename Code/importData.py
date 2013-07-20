9cf15dffa3d1086e20bc7348ba0cdc17fa8b55df
superschedulerapp <superschedulerapp@gmail.com>
2013-07-20 13:02:23 -0400

    deleted:    Code/BotTalking.py 	deleted:    Code/Commands.txt 	deleted:    Code/HighScores.py 	deleted:    Code/MasterBotter.py 	deleted:    Code/Runcode.py 	deleted:    Code/__init__.py 	deleted:    Code/cmds.py 	deleted:    Code/importData.py 	deleted:    Code/zipIt.py

--- Code/importData.py
+++
@@ -1,8 +0,0 @@
-import os
-
-def initData():
-    if not os.path.isdir('./UserData/'):
-        os.mkdir('./UserData/')
-    if not os.path.isfile('./UserData/Data.txt'):
-        for item in ['ChannelsPickle','UsersPickle','BotsPickle','LoginInfoPickle','LinksPickle','Data.txt']:
-            os.system('cp ../NewData/'+item+' '+item+'\n')
